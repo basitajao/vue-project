234fa6e0ff6b3a92aa6050435cf4b8fd1cf109c6
Basit <basitajao96@gmail.com>
2020-08-01 15:45:12 +0100

    crrated repository for my first wajapa task

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <myname message="Basit Ajao" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myname from './components/myname.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    myname
   }
 }
 </script>
---
+++ src/components/myname.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>{{message}}</h1>
+</template>
+
+<script>
+export default {
+    name: "myname",
+    props:{
+        message: String
+    }
+}
+</script>
