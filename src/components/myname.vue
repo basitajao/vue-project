7668f37c9487dc3612a250278cfa143ac64c79a9
Basit <basitajao96@gmail.com>
2020-08-02 21:50:54 +0100

    updated vue-project

--- src/components/myname.vue
+++ src/components/myname.vue
@@ -1,12 +1,26 @@
 <template>
-    <h1>{{message}}</h1>
+    <h1 class="message">{{message}}</h1>
 </template>
 
 <script>
 export default {
     name: "myname",
     props:{
-        message: String
+        message: String,
     }
 }
 </script>
+
+<style>
+
+body{
+    box-sizing: border-box;
+    height: 100vh;
+    padding: auto;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+</style>
